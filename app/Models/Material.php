9494f014753ebed8598adb78356d9e1356676ef5
Robmursan <2123100486@soy.utj.edu.mx>
2025-03-07 15:59:17 -0600

    yue

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Material extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'materiales';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable =[
+        'id',
+        'nombre',
+        'descripcion',
+        'codigo',
+        'unidad',
+        'precio'
+    ];
 }
