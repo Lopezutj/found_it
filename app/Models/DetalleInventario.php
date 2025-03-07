e108d7bfde7de9368eb57234688d1cbf3d423de7
Lopezutj <2123100485@soy.utj.edu.mx>
2025-03-07 15:53:32 -0600

    Modelo_Lopez

--- app/Models/DetalleInventario.php
+++ app/Models/DetalleInventario.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DetalleInventario extends Model
 {
     //
+    use HasFactory;
+
+    protected $table = 'detalles_inventario';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'user_id',
+        'material_id',
+        'estante_id',
+        'cantidad',
+        'fecha_ingreso',
+        'observaciones',
+    ];
 }
--- app/Models/Estante.php
+++ app/Models/Estante.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Estante extends Model
 {
     //
+    use HasFactory;
+
+    protected $table = 'estante'; //nombre de tabla
+    protected $primaryKey = 'id'; //llave primaria
+
+    protected $fillable=[
+        'user_id',
+        'pasillo',
+        'columna',
+        'fila',
+        'almacen',
+    ];
+
+    //funciones de realacion
+
 }
