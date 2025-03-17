cd5160bbe2b2ce202c029d2ff56407125cc4baee
Lopezutj <2123100485@soy.utj.edu.mx>
2025-03-17 15:35:32 -0600

    update_view

--- resources/views/inventory/gestion_embarques.blade.php
+++ resources/views/inventory/gestion_embarques.blade.php
@@ -97,7 +97,7 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
                                 </button>
-                                <button onclick="openEditModal('MAT001')" class="text-[#2045c2] hover:text-[#ff3333] transition-colors duration-150">
+                                <button onclick="location.assign('{{route('Inventory_edit')}}')" class="text-[#2045c2] hover:text-[#ff3333] transition-colors duration-150">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="stroke: currentColor;">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                     </svg>
